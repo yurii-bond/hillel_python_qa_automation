809594bea1cd70f291b498da40374c9e77189ead
MaksymSlipchenko <maksim.slipchenko@gmail.com>
2023-07-25 11:36:38 +0300

    hw_15

--- Lessons/lecture_20/test_calc.py
+++ Lessons/lecture_20/test_calc.py
@@ -26,3 +26,64 @@ def test_add_two_negative_numbers(_calc):
 def test_division_by_zero(_calc):
     with pytest.raises(ZeroDivisionError):
         _calc.div(3, 0)
+
+def test_mult_two_positive_numbers(_calc):
+    assert _calc.mult(2, 2) == 4
+
+def test_mult_two_negative_numbers(_calc):
+    assert _calc.mult(-2, -2) == 4
+
+def test_mult_number_and_zero(_calc):
+    assert _calc.mult(2, 0) == 0
+
+def test_mult_positive_and_negative_numbers(_calc):
+    assert _calc.mult(2, -2) == -4
+
+def test_div_two_positive_numbers(_calc):
+    assert _calc.div(2, 2) == 1
+
+def test_div_two_negative_numbers(_calc):
+    assert _calc.div(-2, -2) == 1
+
+def test_div_number_and_zero(_calc):
+    assert _calc.div(0, 2) == 0
+
+def test_div_positive_and_negative_numbers(_calc):
+    assert _calc.div(2, -2) == -1
+
+def test_subsctract_two_positive_numbers(_calc):
+    assert _calc.subsctract(2, 2) == 0
+
+def test_subsctract_two_negative_numbers(_calc):
+    assert _calc.subsctract(-2, -2) == 0
+
+def test_subsctract_number_and_zero(_calc):
+    assert _calc.subsctract(0, 2) == -2
+
+def test_subsctract_positive_and_negative_numbers(_calc):
+    assert _calc.subsctract(2, -2) == 4
+
+def test_power_two_positive_numbers(_calc):
+    assert _calc.power(2, 2) == 4
+
+def test_power_two_negative_numbers(_calc):
+    assert _calc.power(-2, -2) == 0.25
+
+def test_power_number_and_zero(_calc):
+    assert _calc.power(2, 0) == 1
+
+def test_power_positive_and_negative_numbers(_calc):
+    assert _calc.power(-2, 2) == 4
+
+def test_square_root_with_positive_number(_calc):
+    assert _calc.square_root(4) == 2
+
+def test_square_root_with_zero(_calc):
+    assert _calc.square_root(0) == 0
+
+def test_square_root_with_float_numbers(_calc):
+    assert _calc.square_root(4.84) == 2.2
+
+def test_square_root_with_negative_number(_calc):
+    with pytest.raises(ValueError):
+        _calc.square_root(-4)
