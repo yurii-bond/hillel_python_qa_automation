87ddf3e08b07ac52fbe1d000c597ea8b21d64c1f
Yurii Bondarenko <yuri.gr.bond@gmail.com>
2023-07-31 20:28:34 +0200

    Lecture 22 added.

---
+++ Lessons/lecture_22/tests/fixtures/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from ....lecture_18.app import calc
+
+
+@pytest.fixture(scope='function')
+def calculator():
+    return calc.Calculator()
+
+
+@pytest.fixture(scope='function')
+def _calculator():
+    return calc.Calculator()
+
+
+@pytest.fixture(scope="function")
+def _calc():
+    print('Fixture object created from subdirectory')
+    return calc.Calculator()
