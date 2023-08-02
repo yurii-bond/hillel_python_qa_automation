ef26e731ba67c4ab9741c6957ccd41788747a842
rimmalor <rimmaloranets@gmail.com>
2023-08-02 17:27:38 +0200

    changes made by Rymma Loranets

--- Lessons/lecture_20/test_calc.py
+++ Lessons/lecture_20/test_calc.py
@@ -85,3 +85,97 @@ def test_sqare_root_for_sixteen(_calc):
 )
 def test_substract(_calc, a, b, result):
     assert _calc.subsctract(a, b) == result
+
+
+def test_add_func(_calc):
+    assert _calc.add(5, 5) == 10
+
+
+def test_add_two_positive_numbers(_calc):
+    assert _calc.add(2, 2) == 4
+
+
+def test_add_two_negative_numbers(_calc):
+    assert _calc.add(-2, -2) == -4
+
+
+def test_add_number_and_zero(_calc):
+    assert _calc.add(2, 0) == 2
+
+
+def test_mult_positive(_calc):
+    assert _calc.mult(2, 2) == 4
+
+
+def test_mult_one_float_number(_calc):
+    assert _calc.add(0.2, 2) == 2.2
+
+
+def test_mult_two_float_number(_calc):
+    assert _calc.mult(0.1, 0.1) == 0.010000000000000002
+
+
+def test_mult_one_negative(_calc):
+    assert _calc.mult(-3, 2) == -6
+
+
+def test_division_positive(_calc):
+    assert _calc.div(6, 2) == 3
+
+
+def test_division_negative(_calc):
+    assert _calc.div(6, -2) == -3.0
+
+
+def test_division_one_float(_calc):
+    assert _calc.div(6.6, 2) == 3.3
+
+
+def test_division_two_floats(_calc):
+    assert _calc.div(0.25, 0.4) == 0.625
+
+
+# @pytest.mark.parametrize("a,b,result", [(10, 2, 8), (-5, -1, -4), (4.5, 2.6, 1.9), (-7.7, 8, -15.7)])
+# def test_subtract_positive(_calc, a, b, result):
+#     assert _calc.subsctract(a, b) == result         # as an option
+
+
+def test_subtract_negative(_calc):
+    assert _calc.subsctract(-5, -1) == -4
+
+def test_subtract_two_floats(_calc):
+    assert _calc.subsctract(4.5, 2.6) == 1.9
+
+def test_subtract_negative_float(_calc):
+    assert _calc.subsctract(-7.7, 8) == -15.7
+
+def test_power_positive(_calc):
+    assert _calc.power(2, 4) == 16
+
+
+def test_power_one_float(_calc):
+    assert _calc.power(2.1, 2) == 4.41
+
+
+def test_power_negative(_calc):
+    assert _calc.power(-2.4, 4) == 33.1776
+
+
+def test_power_two_floats(_calc):
+    assert _calc.power(0.2, 0.1) == 0.8513399225207846
+
+
+def test_square_root_positive(_calc):
+    assert _calc.square_root(16) == 4
+
+
+def test_square_root_positive2(_calc):
+    assert _calc.square_root(4) == 2
+
+
+def test_square_root(_calc):
+    assert _calc.square_root(64) == 8
+
+
+def test_square_root_2(_calc):
+    assert _calc.square_root(100) == 10
